385008930cc0ef42ba04115a3e1ab84a1ba9a83d
demanluandyuki <demanluandyuki@126.com>
2015-03-07 11:41:39 +0800

    upload first project

---
+++ calendar/src/com/joyfulmath/calendar/CalendarMainActivity.java
@@ -0,0 +1,23 @@
+package com.joyfulmath.calendar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CalendarMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.calendar_main, menu);
+        return true;
+    }
+    
+}
